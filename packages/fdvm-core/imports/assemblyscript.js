91252a8b78124312771290d4730b942d95d88145
Brandon Fish <brandon.j.fish@gmail.com>
2018-04-18 22:03:12 -0500

    Update assembly script log to only take the string ptr

--- packages/fdvm-core/imports/assemblyscript.js
+++ packages/fdvm-core/imports/assemblyscript.js
@@ -7,8 +7,10 @@ module.exports = {
         memory = mem
     },
     env: {
-        log: function (ptr, len) {
+        log: function (ptr) {
             const m = new Uint8Array(memory.buffer)
+            var uint32view = new Uint32Array(memory.buffer)
+            var len = uint32view[ptr >> 2];
             const buf = new TextDecoder('utf-16le').decode(m.slice(ptr + 4, ptr + 4 + (len * 2)))
             console.log(buf)
         }
